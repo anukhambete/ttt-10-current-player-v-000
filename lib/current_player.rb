ae3b5b1a0ce647afdcd376be5e61d6845d0027d3
Anuradha Khambete <anukhambete@gmail.com>
2018-03-30 15:08:25 +0000

    automatically backed up by learn

--- lib/current_player.rb
+++ lib/current_player.rb
@@ -1,11 +1,11 @@
 def turn(board)
 counter=0
-  board.each do |i|
-    if i == "X" || i == "O"
-      counter = counter + 1
-    else
-      counter
-    end
+board.each do |i|
+  if i == "X" || i == "O"
+  counter = counter + 1
+  else
+  counter
   end
+end
   counter
 end
